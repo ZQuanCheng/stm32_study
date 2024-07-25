4bfb245f4e5bda99553d16cb54c6c12bbe3118eb
Quancheng Zhang <496810357@qq.com>
2024-07-25 21:25:53 +0800

    update

---
+++ 江科大自化协/学习过程代码/6-13 霍尔传感器测速/Sim_Motor_Out/Hardware/SimMotorOut.h
@@ -0,0 +1,23 @@
+#ifndef __SIM_MOTOR_OUT_H
+#define __SIM_MOTOR_OUT_H
+
+void Sim_Motor_Output_Init(void);
+
+void CH1_ON(void);
+void CH1_OFF(void);
+void CH2_ON(void);
+void CH2_OFF(void);
+void CH3_ON(void);
+void CH3_OFF(void);
+
+void Sim_Motor_Prepare_Comutation(void);
+
+#endif
+
+
+
+
+
+
+
+
