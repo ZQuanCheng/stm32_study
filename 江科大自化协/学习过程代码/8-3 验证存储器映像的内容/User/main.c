493771fbf0baf60d15ccb32818f702952a687183
Quancheng Zhang <496810357@qq.com>
2024-08-11 10:41:25 +0800

    update

--- 江科大自化协/学习过程代码/8-3 验证存储器映像的内容/User/main.c
+++ 江科大自化协/学习过程代码/8-3 验证存储器映像的内容/User/main.c
@@ -3,58 +3,23 @@
 #include "OLED.h"
 #include "MyDMA.h"
 
-uint8_t DataA[] = {0x01, 0x02, 0x03, 0x04};				//定义测试数组DataA，为数据源
-uint8_t DataB[] = {0, 0, 0, 0};							//定义测试数组DataB，为数据目的地
+uint8_t aa = 0x66;				
 
 int main(void)
 {
-	/*模块初始化*/
-	OLED_Init();				//OLED初始化
-	
-	MyDMA_Init((uint32_t)DataA, (uint32_t)DataB, 4);	//DMA初始化，把源数组和目的数组的地址传入
-	
+	/*OLED初始化*/
+	OLED_Init();				
+
 	/*显示静态字符串*/
-	OLED_ShowString(1, 1, "DataA");
-	OLED_ShowString(3, 1, "DataB");
+	OLED_ShowString(1, 1, " aa:0x");
+	OLED_ShowString(2, 1, "&aa:0x");	
 	
 	/*显示数组的首地址*/
-	OLED_ShowHexNum(1, 8, (uint32_t)DataA, 8);
-	OLED_ShowHexNum(3, 8, (uint32_t)DataB, 8);
+	OLED_ShowHexNum(1, 7, aa, 2);             // OLED显示  aa:0x66
+	OLED_ShowHexNum(2, 7, (uint32_t)&aa, 8);  // OLED显示 &aa:0x20000000
+	
 		
 	while (1)
 	{
-		DataA[0] ++;		//变换测试数据
-		DataA[1] ++;
-		DataA[2] ++;
-		DataA[3] ++;
-		
-		OLED_ShowHexNum(2, 1, DataA[0], 2);		//显示数组DataA
-		OLED_ShowHexNum(2, 4, DataA[1], 2);
-		OLED_ShowHexNum(2, 7, DataA[2], 2);
-		OLED_ShowHexNum(2, 10, DataA[3], 2);
-		OLED_ShowHexNum(4, 1, DataB[0], 2);		//显示数组DataB
-		OLED_ShowHexNum(4, 4, DataB[1], 2);
-		OLED_ShowHexNum(4, 7, DataB[2], 2);
-		OLED_ShowHexNum(4, 10, DataB[3], 2);
-		
-		Delay_ms(1000);		//延时1s，观察转运前的现象
-		
-		MyDMA_Transfer();	//使用DMA转运数组，从DataA转运到DataB
-//		for (uint8_t i =0; i < 4; i++)
-//		{
-//			DataB[i] = DataA[i];     //使用CPU手动转运数组，从DataA转运到DataB
-//		}
-		
-		
-		OLED_ShowHexNum(2, 1, DataA[0], 2);		//显示数组DataA
-		OLED_ShowHexNum(2, 4, DataA[1], 2);
-		OLED_ShowHexNum(2, 7, DataA[2], 2);
-		OLED_ShowHexNum(2, 10, DataA[3], 2);
-		OLED_ShowHexNum(4, 1, DataB[0], 2);		//显示数组DataB
-		OLED_ShowHexNum(4, 4, DataB[1], 2);
-		OLED_ShowHexNum(4, 7, DataB[2], 2);
-		OLED_ShowHexNum(4, 10, DataB[3], 2);
-
-		Delay_ms(1000);		//延时1s，观察转运后的现象
 	}
 }
